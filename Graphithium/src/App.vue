61b5d59f2cefd4b559bfb7c78df41ca3550034cf
killex8569 <alexandreanette@gmail.com>
2026-01-17 22:47:03 +0100

    Add: new page

--- Graphithium/src/App.vue
+++ Graphithium/src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/info">info</RouterLink>
       </nav>
     </div>
   </header>
---
+++ Graphithium/src/views/info.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>BIJOUR</h1>
+</template>
